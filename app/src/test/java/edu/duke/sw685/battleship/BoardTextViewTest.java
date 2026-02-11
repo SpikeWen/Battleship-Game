ed0c3bceb4fc1072296ee7b6a5fbc7a5bc8483e9
sw685 <sw685@duke.edu>
2026-02-11 15:37:28 -0500

    task2 done

--- app/src/test/java/edu/duke/sw685/battleship/BoardTextViewTest.java
+++ app/src/test/java/edu/duke/sw685/battleship/BoardTextViewTest.java
@@ -19,31 +19,31 @@ public class BoardTextViewTest {
 
   @Test
   public void test_display_empty_2by2() {
-    String expectedHeader = " 0|1\n";
+    String expectedHeader = "  0|1\n";
     String expectedBody = 
-      "A | A\n" +
-      "B | B\n";
+      "A  |  A\n" +
+      "B  |  B\n";
     emptyBoardHelper(2, 2, expectedHeader, expectedBody);
   }
 
   @Test
   public void test_display_empty_3by2() {
-    String expectedHeader = " 0|1|2\n";
+    String expectedHeader = "  0|1|2\n";
     String expectedBody = 
-      "A | | A\n" +
-      "B | | B\n";
+      "A  | |  A\n" +
+      "B  | |  B\n";
     emptyBoardHelper(3, 2, expectedHeader, expectedBody);
   }
 
   @Test
   public void test_display_empty_3by5() {
-    String expectedHeader = " 0|1|2\n";
+    String expectedHeader = "  0|1|2\n";
     String expectedBody = 
-      "A | | A\n" +
-      "B | | B\n" +
-      "C | | C\n" +
-      "D | | D\n" +
-      "E | | E\n";
+      "A  | |  A\n" +
+      "B  | |  B\n" +
+      "C  | |  C\n" +
+      "D  | |  D\n" +
+      "E  | |  E\n";
     emptyBoardHelper(3, 5, expectedHeader, expectedBody);
   }
 
